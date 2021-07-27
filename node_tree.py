ac0affb22b980af1c403e2023576fe877c82c400
drunkenBug <dominikormann@outlook.com>
2021-07-27 13:28:50 +0200

    cleanup

--- node_tree.py
+++ node_tree.py
@@ -40,15 +40,11 @@ class Player:
     def take_damage(self, damage):
         self.hp -= damage
         if self.hp <= 0:
-            ret =  "died"
             self.position.players.pop(self.name)
-            print(self.position.players)
             self.position = None
             self.energy = 0
             self.energy_regen = 0
             self.max_energy = 0
-            self.world.players.pop(self.name)
-            return ret
 
     def status(self):
         Nhp = self.hp/self.max_hp 
@@ -108,7 +104,7 @@ def cast_hit(self,args):
         target_player = self.position.players[target]
         if self.energy > 20:
             self.energy -=20
-            return target_player.take_damage(5)
+            target_player.take_damage(5)
                 
 
 def rename_node(self,args):
@@ -176,8 +172,6 @@ def get_node_from_path(self,path):
 def go_to_node(self, path):
 
     node = get_node_from_path(self, path)
-    if node == None:
-        return 'cant find that node'
 
     self.position.players.pop(self.name)
     self.position = node
@@ -267,7 +261,7 @@ def play(self):
             
             words = command.split()
 
-            print(execute(self, words))
+            execute(self, words)
 
             if self.hp <= 0:
                 print('you died')
@@ -318,4 +312,7 @@ if __name__ == "__main__":
             player = Player(name,world.root, world)
     play(player)
 
+
+def __getattr__(name):
+    return world.players[name]
 
